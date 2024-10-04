f4aa442dedf3479256968546cb30f9d9486c4581
Prashant Singh <113935621+Prash-is-alive@users.noreply.github.com>
2024-10-04 23:02:36 +0530

    Fixed children's import in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,25 @@
-import Script from "next/script";
 import "./globals.css";
-<html lang="en">
-  <head>
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-      crossOrigin="anonymous"
-    />
-  </head>
-  <body>
-    {children}
-    <Script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-      crossOrigin="anonymous"
-      strategy="lazyOnload"
-    />
-  </body>
-</html>;
+import Script from "next/script";
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <body>
+        {children}
+        <Script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+          crossOrigin="anonymous"
+          strategy="lazyOnload" // Ensures the script is loaded lazily
+        />
+      </body>
+    </html>
+  );
+}
